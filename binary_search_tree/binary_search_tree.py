17ff26140d98febe800197141060f626aba88f16
chrispetito <christopherpetito@gmail.com>
2019-09-18 16:14:32 -0700

    complete contains bst method

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -18,6 +18,7 @@ class BinarySearchTree:
     self.value = value
     self.left = None
     self.right = None
+    self.size = 0
 
   # * `insert` adds the input value to the binary search tree, adhering to the
   # rules of the ordering of elements in a binary search tree.
@@ -50,11 +51,32 @@ class BinarySearchTree:
   # We can stop at the first instance of a value
   # We know it's not found if we get to a node that doesn't have children
   def contains(self, target):
-    pass
+    # if self.value = target, return True
+    if self.value == target:
+      return True
+    # if target is less than self.value...
+    elif target < self.value:
+      #if it is self.left...
+      if self.left:
+        # recurse
+        return self.left.contains(target)
+      # else, return False
+      else:
+        return False
+    # if target is greater than or equal to self.value...
+    elif target >= self.value:
+      #if it is self.right...
+      if self.right:
+        # recurse
+        return self.right.contains(target)
+      # else, return False
+      else:
+        return False
+      
 
   # * `get_max` returns the maximum value in the binary search tree.
   def get_max(self):
-    pass
+    return self.size
 
   # * `for_each` performs a traversal of _every_ node in the tree, executing
   # the passed-in callback function on each tree node value. There is a myriad of ways to
